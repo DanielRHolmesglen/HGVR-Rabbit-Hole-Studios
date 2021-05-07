d228682495d9d21165dfe3207bfa50739ab9e955
WilliamC2020 <68672479+WilliamC2020@users.noreply.github.com>
2021-05-07 11:26:37 +1000

    Temp VR character controller
    added vr character controller for testing purposes

---
+++ LiminalBlankProject/Assets/Scripts/VRCharController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using UnityEngine;
+
+public class VRCharController : MonoBehaviour
+{
+    public float speed;
+    public GameObject head;
+    
+    private CharacterController characterController;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var primaryInput = VRDevice.Device.PrimaryInputDevice;
+
+        if (Input.GetKey(KeyCode.W) || primaryInput.GetButtonDown(VRButton.DPadUp))
+        {
+            var x = head.transform.forward.normalized;
+            movement = new Vector3(x.x * speed, -.1f, x.z * speed);
+
+            
+        }
+        else if (Input.GetKey(KeyCode.S) || primaryInput.GetButtonDown(VRButton.DPadDown))
+        {
+            var x = head.transform.forward.normalized;
+             movement = new Vector3(-x.x * speed, -.1f, -x.z * speed);
+        }
+        else if (Input.GetKey(KeyCode.D) || primaryInput.GetButtonDown(VRButton.DPadRight))
+        {
+            var x = head.transform.right.normalized;
+            movement = new Vector3(x.x * speed, -.1f, x.z * speed);
+        }
+        else if (Input.GetKey(KeyCode.A) || primaryInput.GetButtonDown(VRButton.DPadLeft))
+        {
+            var x = head.transform.right.normalized;
+            movement = new Vector3(-x.x * speed, -.1f, -x.z * speed);
+        }
+        else
+        {
+             movement = new Vector3(0, -.1f, 0);
+        }
+        characterController.Move(movement);
+    }
+}
